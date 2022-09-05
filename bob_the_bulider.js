2cefe9182fdd79fb06abb02cbf389850e430234a
kamilla preeti samuel <106230983+Preetisam@users.noreply.github.com>
2022-09-05 19:36:24 +0530

    Update bob_the_bulider.js

--- bob_the_bulider.js
+++ bob_the_bulider.js
@@ -1,10 +1,12 @@
-(function(){
-    function getNumberofBricks(lengthofWall,breathofWAll,thicknessofWall){
-        const bricksvoulme =25*12*8,
-        const actual_wall
-        let voulumeofWall
-    } {
-        
+(function() {
+
+    function getNumberOfBricks(lengthofWall, breadthOfWall, thicknessOfWall){
+        const volumeOfBrick = 24*12*8;
+        const actual_wall = 0.85;
+        let volumeOfWallinCm = lengthofWall * breadthOfWall * thicknessOfWall * 100 *100*100;
+        return Math.ceil((actual_wall * volumeOfWallinCm)/volumeOfBrick);
     }
-}
-    )
+
+    console.log(getNumberOfBricks(24,8, 0.6))
+
+})();
